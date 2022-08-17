3b2a6839dcfb605c36671b8106948d19af44be67
Hiroki Aoyama <etyyuiope@qq.com>
2022-08-17 19:12:15 +0800

    Added theorem proof.

--- Ch1/exercise1.v
+++ Ch1/exercise1.v
@@ -84,7 +84,7 @@ Fixpoint leb (n m : nat) : bool :=
   end.
 
 (* Implementation of <. *)
-(* a < b <=> (a <= b) \/ !(a = b). *)
+(* a < b <=> (a <= b) /\ !(a = b). *)
 Definition ltb (lhs rhs: nat) : bool :=
   match (leb lhs rhs) with
   | false => false
@@ -101,3 +101,42 @@ Example test_ltb2: (ltb 2 4) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_ltb3: (ltb 4 2) = false.
 Proof. simpl. reflexivity. Qed.
+
+(* Proof *)
+Theorem plus_id_exercise : forall n m o : nat,
+  n = m -> m = o -> n + m = m + o.
+Proof.
+  intros n m o.
+  intros H0 H1.
+  rewrite <- H1.
+  rewrite -> H0.
+  (* Apply the hypotheses on both sides of the expression. *)
+  simpl.
+  reflexivity.
+Qed.
+
+(* 0 is also the right-identity element for the additive group. *)
+(* There is a lemma from standard library so we just need to apply transivity. *)
+Lemma mult_0_l : forall n : nat,
+  n * 0 = 0.
+Proof.
+  intros n.
+  rewrite <- mult_n_O.
+  reflexivity.
+Qed.
+
+(* Proof using theorems already proved *)
+Theorem mult_n_1 : forall p : nat,
+  p * 1 = p.
+Proof.
+  intros p.
+  (* p * 1 => p * 0 + p *)
+  rewrite <- mult_n_Sm.
+  (* p * 0 + p=> 0 + p *)
+  rewrite -> mult_0_l.
+  (* 0 +  => p. *)
+  rewrite -> plus_O_n.
+  reflexivity.
+Qed.
+
+Check mult_n_1.
