1654ec566ea0029d8d26e0b4dc9682ee6c581ee3
Hiroki Aoyama <etyyuiope@qq.com>
2022-08-21 14:57:40 +0800

    Finished exercise3.

--- Ch1/exercise3.v
+++ Ch1/exercise3.v
@@ -97,10 +97,8 @@ Have a look at the tests to understand what these functions should do. *)
 Fixpoint nonzeros (l : natlist) : natlist :=
   match l with
   | nil => nil
-  | cons n l' => match n with
-                 | O => nonzeros l'
-                 | _ => cons n (nonzeros l') 
-                 end
+  | cons O l'     => nonzeros l'
+  | cons n l' => cons n (nonzeros l')
   end.
 
 Example test_nonzeros:
@@ -336,4 +334,260 @@ Proof.
   - simpl. rewrite -> app_length. rewrite -> H. apply add_one_right.
 Qed.
 
+
+(** **** Exercise: 3 stars, standard (list_exercises) *)
+Theorem app_nil_r : forall l : natlist,
+  l ++ [] = l.
+Proof.
+  intros l. induction l as [ | n l' H].
+  - reflexivity.
+  - simpl. rewrite -> H. reflexivity.
+Qed.
+
+Theorem rev_app_distr: forall l1 l2 : natlist,
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  intros l1 l2. induction l1 as [| n l' H].
+  - rewrite -> app_nil_r. reflexivity.
+  - simpl. rewrite -> H. rewrite -> app_assoc. reflexivity.
+Qed.
+
+(* An involution is a function that is its own inverse. That is, applying the function twice yield the
+original input. *)
+Theorem rev_involutive : forall l : natlist,
+  rev (rev l) = l.
+Proof.
+  intros l. induction l as [| n l' H].
+  - reflexivity.
+  - simpl. rewrite -> rev_app_distr. rewrite -> H. reflexivity.
+Qed.
+
+(* There is a short solution to the next one. If you find yourself getting tangled up, step back and try
+to look for a simpler way. *)
+Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+Proof.
+  (* Apply association twice. *)
+  intros.
+  rewrite <- app_assoc. rewrite <- app_assoc. reflexivity.
+Qed.
+
+(* An exercise about your implementation of nonzeros: *)
+Lemma nonzeros_app : forall l1 l2 : natlist,
+  nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
+Proof.
+  intros l1 l2. induction l1 as [| n l' H].
+  - simpl. reflexivity.
+  - simpl. rewrite -> H. destruct l2.
+    + rewrite -> app_nil_r. simpl. rewrite -> app_nil_r. reflexivity.
+    + simpl. destruct n.
+      * reflexivity.
+      * reflexivity.
+Qed.
+
+(** **** Exercise: 2 stars, standard (eqblist) *)
+(* Fill in the definition of eqblist, which compares lists of numbers for equality. Prove that eqblist l
+l yields true for every list l. *)
+Fixpoint eqblist (l1 l2 : natlist) : bool :=
+  match l1, l2 with
+  | nil, nil => true 
+  | _, nil => false
+  | nil, _ => false
+  | cons n1 l1', cons n2 l2' =>
+    match eqb n1 n2 with 
+      | true => (eqblist l1' l2')
+      | false => false
+    end
+  end.
+
+Example test_eqblist1 :
+  (eqblist nil nil = true).
+Proof. simpl. reflexivity. Qed.
+Example test_eqblist2 :
+  eqblist [1;2;3] [1;2;3] = true.
+  Proof. simpl. reflexivity. Qed.
+Example test_eqblist3 :
+  eqblist [1;2;3] [1;2;4] = false.
+Proof. simpl. reflexivity. Qed.
+
+Theorem eqblist_refl : forall l : natlist,
+  true = eqblist l l.
+Proof.
+  intros l. induction l.
+  - reflexivity.
+  - simpl. rewrite <- IHl. rewrite eqb_refl. reflexivity.
+Qed.
+
+
+(** **** Exercise: 1 star, standard (count_member_nonzero) *)
+Theorem count_member_nonzero : forall (s : bag),
+  1 <=? (count 1 (1 :: s)) = true.
+Proof.
+  intros s. destruct s.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+(* The following lemma about leb might help you in the next exercise (it will also be useful in later 
+chapters). *)
+Theorem leb_n_Sn : forall n : nat,
+  n <=? (S n) = true.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+  Qed.
+
+(** **** Exercise: 3 stars, advanced (remove_does_not_increase_count) *)
+Theorem remove_does_not_increase_count: forall (s : bag),
+  (count 0 (remove_one 0 s)) <=? (count 0 s) = true.
+Proof.
+  intros s. induction s as [| n s' H].
+  - reflexivity.
+  - simpl. rewrite <- H. destruct n.
+    + rewrite eqb_refl. rewrite leb_n_Sn. rewrite H. reflexivity.
+    + reflexivity.
+Qed.
+
+
+(** **** Exercise: 3 stars, standard, optional (bag_count_sum) *)
+(* Write down an interesting theorem bag_count_sum about bags involving the functions count and sum, and
+prove it using Coq. (You may find that the difficulty of the proof depends on how you defined count!
+Hint: If you defined count using =? you may find it useful to know that destruct works on arbitrary
+expressions, not just simple identifiers.) *)
+Theorem bag_count_sum: forall s1 s2 : bag,
+  count 0 (sum s1 s2) = (count 0 s1) + (count 0 s2).
+Proof.
+  intros. induction s1 as [| n s1' H].
+  - reflexivity.
+  - destruct n.
+    + simpl. rewrite H. reflexivity.
+    + simpl. apply H.
+Qed.
+
+(** **** Exercise: 3 stars, advanced (involution_injective) *)
+(* Prove that every involution is injective. Involutions were defined above in rev_involutive. An involution injective function is one-to-one: it maps distinct inputs to distinct outputs, without any collisions. *)
+Theorem involution_injective : forall (f : nat -> nat),
+  (forall n : nat, n = f (f n)) -> (forall n1 n2 : nat, f n1 = f n2 -> n1 = n2).
+Proof.
+  intros f H0 n1 n2.
+  intros H1.
+  rewrite H0. rewrite <- H1. apply H0.
+Qed.
+
+(** **** Exercise: 2 stars, advanced (rev_injective) *)
+(* Prove that rev is injective. Do not prove this by induction -- that would be hard. Instead, re-use
+the same proof technique that you used for involution_injective. Do not try to use that exercise
+directly as a lemma: the types are not the same. *)
+Theorem rev_injective : forall (l1 l2 : natlist),
+  rev l1 = rev l2 -> l1 = l2.
+Proof.
+  intros l1 l2 H.
+  rewrite <- rev_involutive. rewrite <- H. rewrite -> rev_involutive. reflexivity.
+Qed. 
+
+Inductive natoption : Type :=
+  | Some (n : nat)
+  | None.
+
+Fixpoint nth_error (l : natlist) (n : nat) : natoption :=
+  match l with
+  | nil => None
+  | cons n' l'
+    => match n with
+       | 0 => (Some n')
+       | S next => (nth_error l' next)
+       end
+  end.
+
+(* unwrap_or *)
+Definition option_elim (d : nat) (o : natoption) : nat :=
+  match o with
+  | Some n' => n'
+  | None => d
+  end.
+
+Definition hd_error (l : natlist) : natoption :=
+  match l with
+  | nil => None
+  | cons n _ => Some n
+  end.
+
+Example test_hd_error1 : hd_error [] = None.
+Proof. simpl. reflexivity. Qed.
+Example test_hd_error2 : hd_error [1] = Some 1.
+Proof. simpl. reflexivity. Qed.
+Example test_hd_error3 : hd_error [5;6] = Some 5.
+Proof. simpl. reflexivity. Qed.
+
+(** **** Exercise: 1 star, standard, optional (option_elim_hd) *)
+(* This exercise relates your new hd_error to the old hd. *)
+Theorem option_elim_hd : forall (l : natlist) (default : nat),
+  hd default l = option_elim default (hd_error l).
+Proof.
+  intros. destruct l.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
 End NatList.
+
+Inductive id : Type :=
+  | Id (n : nat).
+
+Definition eqb_id (x1 x2 : id) :=
+  match x1, x2 with
+  | Id n1, Id n2 => n1 =? n2
+  end.
+
+(** **** Exercise: 1 star, standard (eqb_id_refl) *)
+Theorem eqb_id_refl : forall x,
+  eqb_id x x = true.
+Proof.
+  intros x. destruct x as [n].
+  destruct n as [| n'].
+  - reflexivity.
+  - simpl. rewrite eqb_refl. reflexivity.
+Qed.
+
+Module PartialMap.
+Export NatList.
+
+Inductive partial_map : Type :=
+  | empty
+  | record (i : id) (v : nat) (m : partial_map).
+
+
+(* The update function overrides the entry for a given key in a partial map by shadowing it with a new one
+(or simply adds a new entry if the given key is not already present). *)
+Definition update (d : partial_map)
+                  (x : id) (value : nat)
+                  : partial_map :=
+  record x value d.
+
+Fixpoint find (x : id) (d : partial_map) : natoption :=
+  match d with
+  | empty => None
+  | record y v d' => if eqb_id x y
+                      then Some v
+                      else find x d'
+  end.
+
+(* Exercise: 1 star, standard (update_eq) *)
+Theorem update_eq :
+  forall (d : partial_map) (x : id) (v: nat),
+    find x (update d x v) = Some v.
+Proof.
+  intros. simpl. rewrite -> eqb_id_refl. reflexivity.
+Qed.
+
+
+(** **** Exercise: 1 star, standard (update_neq) *)
+Theorem update_neq :
+  forall (d : partial_map) (x y : id) (o: nat),
+    eqb_id x y = false -> find x (update d y o) = find x d.
+Proof.
+  intros. simpl. rewrite -> H. reflexivity.
+Qed.
+
+End PartialMap.
